852aca5fc92edb4f115a663c40d7e138bfb80e5a
Christopher Buckley <cbuckley@microsoft.com>
2024-07-29 15:58:18 +1000

    Updated agent.py will branch off from here

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -10,7 +10,21 @@ from typing import List, Optional
 # Need to make how many iterations to do a certain thing, i.e. how many times to call a tool or agent
 # Memory - what memory should be relevant, i.e. during self learning, the agent SHOULD remember what it's learned etc.
 
-class Observability:
+# Configurations
+
+class ObservabilityConfiguration:
+    pass
+
+class SelfLearningConfiguration:
+    pass
+
+class FactCheckingConfiguration:
+    pass
+
+class NetworkConfiguration:
+    pass
+
+class GroundednessConfiguration:
     pass
 
 class Tool:
@@ -19,47 +33,52 @@ class Tool:
 class Agent:
     def __init__(
         self,
-        id: str, # The unique identifier of the agent
-        model, # The model that the agent uses to generate responses
-        version: str, # The version of the agent
-        name: str, # The name of the agent
-        responsibility: str, # The responsibility of the agent
-        instructions: str, # Instructions on how to achieve the goal
-        recommended_process: List[str], # List of steps that the agent should follow to achieve it's goal
-        tools: List[Tool], # List of tools that the agent can use to achieve it's goal
-        temperature: float, # Temperature controls the randomness of the model's output by adjusting the probability distribution, with lower values producing more focused results and higher values encouraging more diverse and creative outputs.
-        top_p: float, # Top p (Nucleus Sampling): Top p determines the size of the token pool for selection by choosing the smallest set of tokens whose cumulative probability exceeds the specified value, with lower values focusing on more probable tokens and higher values allowing for more diverse selections.
-        observability: Observability = None, # Takes an observability object which contains information...
-        input_guards: List['Agent'] = None, # Agents can be used as a guard for the input of this agent
-        output_guards: List['Agent'] = None, # Agents can be used as a guard for the output of this agent
-        self_reflection: bool = False, # Agent can reflect on it's own actions and learn from them
-        feedback: List['Agent'] = None, # Agent can receive feedback from other agents
-        self_learning: bool = False, # Before the agent takes an action, it can learn what it needs to execute the goal better
-        self_learning_instructions: str = None, # Instructions on how to learn
-        self_learning_tools: List[Tool] = None, # Tools that can be used to help the agent learn
-        
-        fact_check = None, # Fact checking can be used to verify the information that the agent is using
-        ):
+        id: str,  # The unique identifier of the agent
+        model,  # The model that the agent uses to generate responses
+        version: str,  # The version of the agent
+        name: str,  # The name of the agent
+        persona: str,  # The persona of the agent (system message)
+        responsibility: str,  # The responsibility of the agent
+        instructions: str,  # Instructions on how to achieve the goal
+        recommended_process: List[str],  # List of steps that the agent should follow to achieve its goal
+        examples: List[str],  # Few-shot examples
+        prompt_override: Optional[str] = None,  # (user message) If set this will override the responsibility, instructions, recommended_process, and examples.
+        tools: List['Tool'] = [],  # List of tools that the agent can use to achieve its goal
+        temperature: float = 1.0,  # Temperature controls the randomness of the model's output
+        top_p: float = 1.0,  # Top p (Nucleus Sampling): Determines the size of the token pool for selection
+        self_learning: Optional['SelfLearningConfiguration'] = None,  # Before the agent takes an action, it can learn what it needs to execute the goal better
+        fact_check: Optional['FactCheckingConfiguration'] = None,  # Fact checking can be used to verify the information that the agent is using
+        network: Optional['NetworkConfiguration'] = None,  # Network configuration for the agent
+        groundedness_check: Optional['GroundednessConfiguration'] = None,  # Groundedness check for the agent
+        observability: Optional['ObservabilityConfiguration'] = None,  # Takes an observability object which contains information...
+        input_guards: Optional[List['Agent']] = None,  # Agents can be used as a guard for the input of this agent
+        output_guards: Optional[List['Agent']] = None,  # Agents can be used as a guard for the output of this agent
+        self_reflection: bool = False,  # Agent can reflect on its own actions and learn from them
+        feedback: Optional[List['Agent']] = None,  # Agent can receive feedback from other agents
+    ):
         self.id = id
         self.model = model
         self.version = version
         self.name = name
+        self.persona = persona
         self.responsibility = responsibility
         self.instructions = instructions
         self.recommended_process = recommended_process
+        self.examples = examples
+        self.prompt_override = prompt_override
         self.tools = tools
         self.temperature = temperature
         self.top_p = top_p
+        self.self_learning = self_learning
+        self.fact_check = fact_check
+        self.network = network
+        self.groundedness_check = groundedness_check
         self.observability = observability
         self.input_guards = input_guards
         self.output_guards = output_guards
         self.self_reflection = self_reflection
         self.feedback = feedback
-        self.self_learning = self_learning
-        self.self_learning_instructions = self_learning_instructions
-        self.self_learning_tools = self_learning_tools
-        
-        self.fact_check = fact_check
+
 
 
 class AgentOperation:
